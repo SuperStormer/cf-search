f7fc393dd209d1546bd11f910cd477c5a66996ba
Larry Xue <larry.p.xue@gmail.com>
2022-09-19 16:26:29 -0400

    workaround for https://github.com/clean-css/clean-css-cli/issues/80

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ if [ "$NODE_ENV" = "production" ]; then
 	html-minifier-terser --conservative-collapse --collapse-whitespace src/index.html -o dist/index.html
 	rsync -av --ignore-existing --exclude scripts --exclude styles --exclude index.html src/ dist/
 else 
+	# note: you need to apply https://github.com/clean-css/clean-css-cli/pull/81 manually to node_modules/clean-css-cli
+	# because the maintainer isn't responding
 	cleancss -O0 -o dist/index.css src/styles/index.css
 	rsync -av  --ignore-existing --exclude scripts --exclude styles src/ dist/
 fi
