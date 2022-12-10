6eac08af17dbcdf59ea9032503b6afabfd49d6c6
Larry Xue <larry.p.xue@gmail.com>
2022-12-10 16:36:35 -0500

    fix max/min ver filters

--- src/scripts/filters.js
+++ src/scripts/filters.js
@@ -82,7 +82,7 @@ export function filter_results(results, filters) {
 			max_ver += "a";
 		}
 		results = results.filter((result) =>
-			result.latestFilesIndexes.every(
+			result.latestFilesIndexes.some(
 				(file) => natural_compare(file.gameVersion, max_ver) <= 0
 			)
 		);
@@ -91,7 +91,7 @@ export function filter_results(results, filters) {
 	//handle min ver
 	if (min_ver.length > 0) {
 		results = results.filter((result) =>
-			result.latestFilesIndexes.every(
+			result.latestFilesIndexes.some(
 				(file) => natural_compare(file.gameVersion, min_ver) >= 0
 			)
 		);
